446e579bbf19c602d7a39fafe2a45f1b29b66eef
Tom Vervecken <tomvervecken@MacBook-Pro-van-Tom.local>
2023-12-09 00:40:25 +0100

    first-commit

---
+++ src/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <main className="About">
+        <h2>About</h2>
+        <p style={{ marginTop: "1rem" }}>This Blog App is a project in the learn React tutorial series.</p>
+    </main>
+  )
+}
+
+export default About
